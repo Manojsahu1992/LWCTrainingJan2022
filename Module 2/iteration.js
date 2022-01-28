22837e25a657a8f2ce2a797436267c393e1407aa
Manojsahu1992 <98113221+Manojsahu1992@users.noreply.github.com>
2022-01-28 16:33:20 +0530

    Add files via upload

---
+++ Module 2/iteration.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+export default class Iteration extends LightningElement {}
+Students=[
+    {
+        Sid:101,
+        Name:'john Abraham',
+        Email:'ja@gmail.com'
+    },
+    {
+        Sid:102,
+        Name:'Brad pitt',
+        Email:'bpitt@gmail.com'
+    },
+    {
+        Sid:103,
+        Name:'SwatiAngelina Jolie',
+        Email:'angelie@gmail.com'
+    },
+    {
+        Sid:104,
+        Name:'Peter Parker',
+        Email:'spiderman@marvelcomics.com'
+    }
+];
+}
